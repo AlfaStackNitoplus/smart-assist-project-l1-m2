1b445baf916826101f8e319e0d11a1c4961d7efe
AlfaStackNitoplus <alfastack.nitoplus@gmail.com>
2026-01-16 17:01:48 +0530

    code commited

---
+++ docs/login.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  username = '';
+  password = '';
+  constructor(private router: Router) { }
+
+
+  onLogin() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // call your login service here
+    this.router.navigate(['/user']);
+  }
+}
